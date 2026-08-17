73dfe9ef20f003b049e9bc6ff195e3c8ec128dce
Rafael Alves <rafal.a.lima@edu.ufes.br>
2026-08-17 14:58:43 -0300

    Configures h2database and JPA

--- src/main/java/com/rafaelalves/course/entities/User.java
+++ src/main/java/com/rafaelalves/course/entities/User.java
@@ -1,14 +1,15 @@
 package com.rafaelalves.course.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
